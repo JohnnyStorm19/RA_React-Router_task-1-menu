08b9dd6bdacf59ac7ca63d09a05847e50a514772
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-12-20 11:53:39 +0300

    fix: added basename to BrowserRouter component

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/RA_React-Router_task-1-menu'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
